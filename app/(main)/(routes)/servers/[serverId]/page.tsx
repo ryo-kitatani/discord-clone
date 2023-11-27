49b53a8da24cc11065943365061649ec784e1310
ryo-kitatani <ryo0929m@gmail.com>
2023-11-28 02:42:41 +0900

    add sidebar icon

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,9 @@
+const serverIdPage = () => {
+  return (
+    <div>
+      <h1>Server Page</h1>
+    </div>
+  );
+}
+
+export default serverIdPage;
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,10 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import {
   Dialog,
@@ -35,6 +37,7 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -50,7 +53,15 @@ export const InitialModal = () => {
 
   const isLoaded = form.formState.isSubmitting;
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    try {
+     await axios.post('/api/servers', values);
+
+     form.reset();
+     router.refresh();
+     window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   if (!isMounted) {
